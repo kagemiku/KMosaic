dacac4711b30f8a197aef1ba9be8ce8a174460b5
kage-mac <kagemiku@gmail.com>
2015-11-17 17:04:56 +0900

    Use c++11 style declaration of variables.

--- ImageConverter.cpp
+++ ImageConverter.cpp
@@ -17,17 +17,17 @@ auto ImageConverter::resizeImage(const cv::Mat& source, cv::Mat& destination, in
 
 auto ImageConverter::calcMeanColor(const cv::Mat& source, int blockWidth, int blockHeight, int row, int col) -> ImageConverter::RGB
 {
-	int r = 0, g = 0, b = 0;
-	for ( int i = row*blockHeight; i < (row + 1)*blockHeight; i++ ) {
-		for ( int j = col*blockWidth; j < (col + 1)*blockWidth; j++ ) {
-			int baseIdx = source.step*i + COLOR_CHANNEL*j;
+	auto r = 0, g = 0, b = 0;
+	for ( auto i = row*blockHeight; i < (row + 1)*blockHeight; i++ ) {
+		for ( auto j = col*blockWidth; j < (col + 1)*blockWidth; j++ ) {
+			auto baseIdx = source.step*i + COLOR_CHANNEL*j;
 			r += source.data[baseIdx + RED_IDX];
 			g += source.data[baseIdx + GREEN_IDX];
 			b += source.data[baseIdx + BLUE_IDX];
 		}
 	}
 
-	const int numPixel = blockWidth*blockHeight;
+	const auto numPixel = blockWidth*blockHeight;
 	r = static_cast<int>(static_cast<double>(r)/static_cast<double>(numPixel));
 	g = static_cast<int>(static_cast<double>(g)/static_cast<double>(numPixel));
 	b = static_cast<int>(static_cast<double>(b)/static_cast<double>(numPixel));
@@ -38,9 +38,9 @@ auto ImageConverter::calcMeanColor(const cv::Mat& source, int blockWidth, int bl
  
 auto ImageConverter::drawMeanColor(cv::Mat& destination, int blockWidth, int blockHeight, int row, int col, const RGB& color) -> void
 {
-	for ( int i = row*blockHeight; i < (row + 1)*blockHeight; i++ ) {
-		for ( int j = col*blockWidth; j < (col + 1)*blockWidth; j++ ) {
-			int baseIdx = destination.step*i + COLOR_CHANNEL*j;
+	for ( auto i = row*blockHeight; i < (row + 1)*blockHeight; i++ ) {
+		for ( auto j = col*blockWidth; j < (col + 1)*blockWidth; j++ ) {
+			auto baseIdx = destination.step*i + COLOR_CHANNEL*j;
 			destination.data[baseIdx + RED_IDX]		= color.r;
 			destination.data[baseIdx + GREEN_IDX]	= color.g;
 			destination.data[baseIdx + BLUE_IDX]	= color.b;
@@ -51,11 +51,11 @@ auto ImageConverter::drawMeanColor(cv::Mat& destination, int blockWidth, int blo
 
 auto ImageConverter::dropTone(const cv::Mat& source, cv::Mat& destination, int blockRows, int blockCols) -> void
 {
-	const int blockWidth	= destination.size().width/blockCols;
-	const int blockHeight	= destination.size().height/blockRows;
-	for ( int r = 0; r < blockRows; r++ ) {
-		for ( int c = 0; c < blockCols; c++ ) {
-			const RGB color = calcMeanColor(source, blockWidth, blockHeight, r, c);
+	const auto blockWidth	= destination.size().width/blockCols;
+	const auto blockHeight	= destination.size().height/blockRows;
+	for ( auto r = 0; r < blockRows; r++ ) {
+		for ( auto c = 0; c < blockCols; c++ ) {
+			const auto color = calcMeanColor(source, blockWidth, blockHeight, r, c);
 			drawMeanColor(destination, blockWidth, blockHeight, r, c, color);
 		}
 	}
@@ -68,7 +68,7 @@ auto ImageConverter::convertImage(const cv::Mat& source, cv::Mat& destination, i
 		throw std::string("invalid params");
 	}
 
-	cv::Mat tmp = destination.clone();
+	auto tmp = destination.clone();
 	resizeImage(source, tmp, width, height);
 	dropTone(tmp, destination, blockRows, blockCols);
 }
