243d4fca0731a775983dea3a8186d9ccca6902ec
kage-mac <kagemiku@gmail.com>
2015-11-17 18:05:35 +0900

    Modify main.cpp
    Because its code don't use 4-space but tab.

--- examples/main.cpp
+++ examples/main.cpp
@@ -11,22 +11,22 @@ auto main() -> int
     const int blockRows = 10;
     const int blockCols = 10;
 
-	string srcFilePath("image.png");
-	cv::Mat src = cv::imread(srcFilePath);
-	if ( src.empty() ) {
-		cerr << srcFilePath << " can't be read." << endl;
-		exit(1);
-	}	
+    string srcFilePath("image.png");
+    cv::Mat src = cv::imread(srcFilePath);
+    if ( src.empty() ) {
+        cerr << srcFilePath << " can't be read." << endl;
+        exit(1);
+    }	
 
-	cv::Mat dest = cv::Mat::zeros(cv::Size(width, height), CV_8UC3);
-	try { 
-		ImageConverter::convertImage(src, dest, width, height, blockRows, blockCols);
-	} catch ( const string& msg ) {
-		cerr << msg << endl;
-		exit(1);
-	}
+    cv::Mat dest = cv::Mat::zeros(cv::Size(width, height), CV_8UC3);
+    try { 
+        ImageConverter::convertImage(src, dest, width, height, blockRows, blockCols);
+    } catch ( const string& msg ) {
+        cerr << msg << endl;
+        exit(1);
+    }
 
     // write some processes...
 	
-	return 0;
+    return 0;
 }
