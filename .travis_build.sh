f57d87c4d46c8e441b2f352da3a5344f6e2a6c4f
kage-mac <kagemiku@gmail.com>
2015-12-27 15:52:12 +0900

    Add setting files for Travis-CI.

---
+++ .travis_build.sh
@@ -0,0 +1,19 @@
+echo "Generating googletest libraly"
+clang++ -std=c++1y -pthread -g -Wall -Wextra -isystem tests/googletest/googletest/include -isystem tests/googletest/googletest/ -c -o tests/gtest-all.o tests/googletest/googletest/src/gtest-all.cc
+echo "Done compiling gtest-all.cc"
+ar -rv tests/libgtest.a tests/gtest-all.o
+echo "Done archiving gtest-all.o"
+echo "Compiling sources..."
+clang++ -std=c++1y -pthread -g -Wall -Wextra -isystem opencv/build/include -c -o tests/kmosaic.o sources/kmosaic.cpp
+echo "Compiling unit tests..."
+clang++ -std=c++1y -pthread -g -Wall -Wextra -isystem tests/googletest/googletest/include -c -o tests/main.o tests/main.cpp
+echo "Done compiling main.cpp"
+clang++ -std=c++1y -pthread -g -Wall -Wextra -isystem tests/googletest/googletest/include -isystem opencv/build/include -c -o tests/kmosaic_test.o tests/kmosaic_test.cpp
+echo "Done compiling node_test.cpp"
+clang++ -std=c++1y -g -Wall -Wextra -o tests/gtest tests/kmosaic.o tests/main.o tests/kmosaic_test.o -pthread -Ltests/ -Lopencv/build/lib -lgtest -lopencv_highgui -lopencv_core -lopencv_imgproc
+echo "Running unit tests..."
+tests/gtest -v
+result=$?
+rm -r tests/gtest tests/kmosaic.o tests/main.o tests/kmosaic_test.o tests/gtest-all.o tests/libgtest.a
+echo "Unit tests completed : $result"
+exit $result
